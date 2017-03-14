b86bb11ed32d04de04ee047ed10eb9af94ceadc1
Omar Ansari <omaransari1@gmail.com>
2017-03-14 18:00:31 -0500

    Change ngforce expression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  NG_FORCE=true RAILS_ENV=development bundle exec rake assets:clobber assets:precompile
+  NG_FORCE=true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
